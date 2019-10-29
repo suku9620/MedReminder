5f65e824356189f7213302120d2dfb16fa21c054
Vishnu Sukumaran <sukumaranvishnu@gmail.com>
2019-10-28 22:47:03 -0400

    UI Changes complete

--- app/src/main/java/wilfridlaurier/vishnusukumaran/medtracker/AddReminder.java
+++ app/src/main/java/wilfridlaurier/vishnusukumaran/medtracker/AddReminder.java
@@ -1,6 +1,37 @@
 package wilfridlaurier.vishnusukumaran.medtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.app.LoaderManager;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+import java.util.Calendar;
 
 import android.os.Bundle;
 
--- app/src/main/java/wilfridlaurier/vishnusukumaran/medtracker/MainActivity.java
+++ app/src/main/java/wilfridlaurier/vishnusukumaran/medtracker/MainActivity.java
@@ -1,14 +1,31 @@
 package wilfridlaurier.vishnusukumaran.medtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.content.Intent;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton mAddReminderButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void createReminder(View view) {
+        mAddReminderButton = (FloatingActionButton) findViewById(R.id.plus_button);
+
+        mAddReminderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), AddReminder.class);
+                startActivity(intent);
+            }
+        });
     }
 }
