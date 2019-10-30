87043fbc5810a38476cadc75d29523a6beab239b
Vishnu Sukumaran <sukumaranvishnu@gmail.com>
2019-10-29 23:54:56 -0400

    29-10-2019 changes

--- app/src/main/java/wilfridlaurier/vishnusukumaran/medtracker/AddReminder.java
+++ app/src/main/java/wilfridlaurier/vishnusukumaran/medtracker/AddReminder.java
@@ -11,6 +11,7 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import androidx.fragment.app.FragmentActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -29,17 +30,307 @@ import android.widget.Toast;
 
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
 import java.util.Calendar;
 
+import java.util.Calendar;
+import java.util.Calendar;
 import android.os.Bundle;
 
-public class AddReminder extends AppCompatActivity {
+public class AddReminder extends AppCompatActivity  {
+
+    //Initialize
+
+    private Toolbar mToolbar;
+    private EditText mTitleText;
+    private TextView mDateText, mTimeText, mRepeatText, mRepeatNoText, mRepeatTypeText;
+    private FloatingActionButton mFAB1;
+    private FloatingActionButton mFAB2;
+    private Calendar mCalendar;
+    private int mYear, mMonth, mHour, mMinute, mDay;
+    private long mRepeatTime;
+    private Switch mRepeatSwitch;
+    private String mTitle;
+    private String mTime;
+    private String mDate;
+    private String mRepeat;
+    private String mRepeatNo;
+    private String mRepeatType;
+    private String mActive;
+
+    //constant to know if the data is changed
+    private boolean mVehicleHasChanged = false;
+    // Constant values in milliseconds
+    private static final long milMinute = 60000L;
+    private static final long milHour = 3600000L;
+    private static final long milDay = 86400000L;
+    private static final long milWeek = 604800000L;
+    private static final long milMonth = 2592000000L;
+
+
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            mVehicleHasChanged = true;
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reminder);
+
+
+        // Initialize Views
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mTitleText = (EditText) findViewById(R.id.reminder_title);
+        mDateText = (TextView) findViewById(R.id.set_date);
+        mTimeText = (TextView) findViewById(R.id.set_time);
+        mRepeatText = (TextView) findViewById(R.id.set_repeat);
+        mRepeatNoText = (TextView) findViewById(R.id.set_repeat_no);
+        mRepeatTypeText = (TextView) findViewById(R.id.set_repeat_type);
+        mRepeatSwitch = (Switch) findViewById(R.id.repeat_switch);
+        mFAB1 = (FloatingActionButton) findViewById(R.id.starred1);
+        mFAB2 = (FloatingActionButton) findViewById(R.id.starred2);
+
+        // Initialize default values
+        mActive = "true";
+        mRepeat = "true";
+        mRepeatNo = Integer.toString(1);
+        mRepeatType = "Hour";
+
+        mCalendar = Calendar.getInstance();
+        mHour = mCalendar.get(Calendar.HOUR_OF_DAY);
+        mMinute = mCalendar.get(Calendar.MINUTE);
+        mYear = mCalendar.get(Calendar.YEAR);
+        mMonth = mCalendar.get(Calendar.MONTH) + 1;
+        mDay = mCalendar.get(Calendar.DATE);
+
+        mDate = mDay + "/" + mMonth + "/" + mYear;
+        mTime = mHour + ":" + mMinute;
+
+        // Setup Reminder Title EditText
+        mTitleText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mTitle = s.toString().trim();
+                mTitleText.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        // Setup TextViews using reminder values
+        mDateText.setText(mDate);
+        mTimeText.setText(mTime);
+        mRepeatNoText.setText(mRepeatNo);
+        mRepeatTypeText.setText(mRepeatType);
+        mRepeatText.setText("Every " + mRepeatNo + " " + mRepeatType + "(s)");
+
+        // Setup up active buttons
+        if (mActive.equals("false")) {
+            findViewById(R.id.starred1).setVisibility(View.VISIBLE);
+            findViewById(R.id.starred2).setVisibility(View.GONE);
+
+        } else if (mActive.equals("true")) {
+            findViewById(R.id.starred1).setVisibility(View.GONE);
+            findViewById(R.id.starred2).setVisibility(View.VISIBLE);
+        }
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(R.string.title_activity_add_reminder);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
     }
+    // On clicking Time picker
+    public void setTime(View v){
+        Calendar now = Calendar.getInstance();
+        TimePickerDialog tpd = TimePickerDialog.newInstance(
+                (TimePickerDialog.OnTimeSetListener) this,
+                now.get(Calendar.HOUR_OF_DAY),
+                now.get(Calendar.MINUTE),
+                false
+        );
+        tpd.setThemeDark(false);
+        tpd.show(getFragmentManager(), "Timepickerdialog");
+
+    }
+    // On clicking Date picker
+    public void setDate(View v){
+        Calendar now = Calendar.getInstance();
+        DatePickerDialog dpd = DatePickerDialog.newInstance(
+                (DatePickerDialog.OnDateSetListener) this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+        );
+        dpd.show(getFragmentManager(), "Datepickerdialog");
+    }
+    // Obtain time from time picker
+
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+        mHour = hourOfDay;
+        mMinute = minute;
+        if (minute < 10) {
+            mTime = hourOfDay + ":" + "0" + minute;
+        } else {
+            mTime = hourOfDay + ":" + minute;
+        }
+        mTimeText.setText(mTime);
+    }
+
+    // Obtain date from date picker
+
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        monthOfYear ++;
+        mDay = dayOfMonth;
+        mMonth = monthOfYear;
+        mYear = year;
+        mDate = dayOfMonth + "/" + monthOfYear + "/" + year;
+        mDateText.setText(mDate);
+    }
+    // On clicking the active button
+    public void selectFab1(View v) {
+        mFAB1 = (FloatingActionButton) findViewById(R.id.starred1);
+        findViewById(R.id.starred1).setVisibility(View.GONE);
+        mFAB2 = (FloatingActionButton) findViewById(R.id.starred2);
+        findViewById(R.id.starred1).setVisibility(View.VISIBLE);
+        mActive = "true";
+    }
+    // On clicking the inactive button
+    public void selectFab2(View v) {
+        mFAB2 = (FloatingActionButton) findViewById(R.id.starred2);
+        findViewById(R.id.starred1).setVisibility(View.GONE);
+        mFAB1 = (FloatingActionButton) findViewById(R.id.starred1);
+        findViewById(R.id.starred1).setVisibility(View.VISIBLE);
+        mActive = "false";
+    }
+    // On clicking the repeat switch
+    public void onSwitchRepeat(View view) {
+        boolean on = ((Switch) view).isChecked();
+        if (on) {
+            mRepeat = "true";
+            mRepeatText.setText("Every " + mRepeatNo + " " + mRepeatType + "(s)");
+        } else {
+            mRepeat = "false";
+            mRepeatText.setText(R.string.repeat_off);
+        }
+    }
+    // On clicking repeat type button
+    public void selectRepeatType(View v){
+        final String[] items = new String[5];
+
+        items[0] = "Minute";
+        items[1] = "Hour";
+        items[2] = "Day";
+        items[3] = "Week";
+        items[4] = "Month";
+
+        // Create List Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Type");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int item) {
+
+                mRepeatType = items[item];
+                mRepeatTypeText.setText(mRepeatType);
+                mRepeatText.setText("Every " + mRepeatNo + " " + mRepeatType + "(s)");
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    // On clicking repeat interval button
+    public void setRepeatNo(View v){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Enter Number");
+
+        // Create EditText box to input repeat number
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        alert.setView(input);
+        alert.setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+
+                        if (input.getText().toString().length() == 0) {
+                            mRepeatNo = Integer.toString(1);
+                            mRepeatNoText.setText(mRepeatNo);
+                            mRepeatText.setText("Every " + mRepeatNo + " " + mRepeatType + "(s)");
+                        }
+                        else {
+                            mRepeatNo = input.getText().toString().trim();
+                            mRepeatNoText.setText(mRepeatNo);
+                            mRepeatText.setText("Every " + mRepeatNo + " " + mRepeatType + "(s)");
+                        }
+                    }
+                });
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // do nothing
+            }
+        });
+        alert.show();
+    }
+    private void showUnsavedChangesDialog(
+            DialogInterface.OnClickListener discardButtonClickListener) {
+        // Create an AlertDialog.Builder and set the message, and click listeners
+        // for the postivie and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.unsaved_changes_dialog_msg);
+        builder.setPositiveButton(R.string.discard, discardButtonClickListener);
+        builder.setNegativeButton(R.string.keep_editing, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Keep editing" button, so dismiss the dialog
+                // and continue editing the reminder.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void showDeleteConfirmationDialog() {
+        // Create an AlertDialog.Builder and set the message, and click listeners
+        // for the postivie and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.delete_dialog_msg);
+        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Delete" button, so delete the reminder.
+                deleteReminder();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Cancel" button, so dismiss the dialog
+                // and continue editing the reminder.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+    private void deleteReminder() {
+
+    }
+
 }
