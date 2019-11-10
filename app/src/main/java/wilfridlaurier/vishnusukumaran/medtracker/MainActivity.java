4d035867dbca1590cdf473925681b8f16b2868e0
Vishnu Sukumaran <sukumaranvishnu@gmail.com>
2019-11-09 23:30:12 -0500

    09-11-2019 Complete UI changes

--- app/src/main/java/wilfridlaurier/vishnusukumaran/medtracker/MainActivity.java
+++ app/src/main/java/wilfridlaurier/vishnusukumaran/medtracker/MainActivity.java
@@ -15,20 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
-        mToolbar.setTitle(R.string.app_name);
+
 
     }
-    public void createReminder(View view) {
-        mAddReminderButton = (FloatingActionButton) findViewById(R.id.plus_button);
 
-        mAddReminderButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), AddReminder.class);
-                startActivity(intent);
-            }
-        });
     }
-}
+
