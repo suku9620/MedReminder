3d359515f01982ebfb817c2426581bfcd747e0cd
Vishnu Sukumaran <sukumaranvishnu@gmail.com>
2019-11-28 14:32:00 -0500

    28-11-2019 changes

--- app/src/main/java/wilfridlaurier/vishnusukumaran/medtracker/ui/main/PlaceholderFragment.java
+++ app/src/main/java/wilfridlaurier/vishnusukumaran/medtracker/ui/main/PlaceholderFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -17,43 +18,46 @@ import wilfridlaurier.vishnusukumaran.medtracker.R;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class PlaceholderFragment extends Fragment {
-
+public  class PlaceholderFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-    private PageViewModel pageViewModel;
+    ImageView img;
+    String [] title={"Welcome to MedTracker","Place where you put medicine reminders", "Reminders can have date, time and number of repetitions. Lets get started!"};
+    int[] bgs = new int[]{R.mipmap.ic_launcher, R.drawable.ic_access_alarms_black_24dp, R.drawable.ic_vibration_black_24dp};
 
-    public static PlaceholderFragment newInstance(int index) {
+    public PlaceholderFragment() {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static PlaceholderFragment newInstance(int sectionNumber) {
         PlaceholderFragment fragment = new PlaceholderFragment();
-        Bundle bundle = new Bundle();
-        bundle.putInt(ARG_SECTION_NUMBER, index);
-        fragment.setArguments(bundle);
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
-        int index = 1;
-        if (getArguments() != null) {
-            index = getArguments().getInt(ARG_SECTION_NUMBER);
-        }
-        pageViewModel.setIndex(index);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_med_pager, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText(title[ getArguments().getInt(ARG_SECTION_NUMBER)-1]);
+
+        img = (ImageView) rootView.findViewById(R.id.imageView);
+        img.setBackgroundResource(bgs[getArguments().getInt(ARG_SECTION_NUMBER) - 1]);
+
+
+        return rootView;
     }
 
-    @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_med_pager, container, false);
-        final TextView textView = root.findViewById(R.id.section_label);
-        pageViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
-    }
+
+
 }
--- app/src/main/java/wilfridlaurier/vishnusukumaran/medtracker/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/wilfridlaurier/vishnusukumaran/medtracker/ui/main/SectionsPagerAdapter.java
@@ -2,27 +2,28 @@ package wilfridlaurier.vishnusukumaran.medtracker.ui.main;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+
 import wilfridlaurier.vishnusukumaran.medtracker.R;
 
 /**
- * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ /**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-    @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
-    private final Context mContext;
 
-    public SectionsPagerAdapter(Context context, FragmentManager fm) {
-        super(fm);
-        mContext = context;
+    public SectionsPagerAdapter(FragmentManager fm) {
+
+
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
     }
 
     @Override
@@ -30,17 +31,26 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
         return PlaceholderFragment.newInstance(position + 1);
-    }
 
-    @Nullable
-    @Override
-    public CharSequence getPageTitle(int position) {
-        return mContext.getResources().getString(TAB_TITLES[position]);
     }
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        // Show 3 total pages.
+        return 3;
     }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "SECTION 1";
+            case 1:
+                return "SECTION 2";
+            case 2:
+                return "SECTION 3";
+        }
+        return null;
+    }
+
 }
