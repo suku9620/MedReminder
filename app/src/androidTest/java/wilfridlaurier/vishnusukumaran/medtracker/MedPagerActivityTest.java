5d3236cd6d5c2a9a32160440722ca7c951c309e0
Vishnu Sukumaran <sukumaranvishnu@gmail.com>
2019-12-11 15:09:38 -0500

    11-12-2019 changes

---
+++ app/src/androidTest/java/wilfridlaurier/vishnusukumaran/medtracker/MedPagerActivityTest.java
@@ -0,0 +1,202 @@
+package wilfridlaurier.vishnusukumaran.medtracker;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(JUnit4.class)
+public class MedPagerActivityTest {
+
+    @Rule
+    public ActivityTestRule<MedPagerActivity> mActivityTestRule = new ActivityTestRule<>(MedPagerActivity.class);
+
+    @Test
+    public void medPagerActivityTest() {
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.reminder_title),
+                        childAtPosition(
+                                allOf(withId(R.id.add_reminder_layout_top),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("Test"), closeSoftKeyboard());
+
+        ViewInteraction relativeLayout = onView(
+                allOf(withId(R.id.RepeatType),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                6)));
+        relativeLayout.perform(scrollTo(), click());
+
+        DataInteraction appCompatTextView = onData(anything())
+                .inAdapterView(allOf(withClassName(is("com.android.internal.app.AlertController$RecycleListView")),
+                        childAtPosition(
+                                withClassName(is("android.widget.FrameLayout")),
+                                0)))
+                .atPosition(2);
+        appCompatTextView.perform(click());
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.save_reminder), withContentDescription("Save Medicine"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        2),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.recycle_title), withText("Test"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.RelativeLayout.class),
+                                        1),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Test")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.recycle_repeat_info), withText("Every 1 Day(s)"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.RelativeLayout.class),
+                                        1),
+                                2),
+                        isDisplayed()));
+        textView2.check(matches(withText("Every 1 Day(s)")));
+
+        DataInteraction relativeLayout2 = onData(anything())
+                .inAdapterView(allOf(withId(R.id.list),
+                        childAtPosition(
+                                withClassName(is("android.widget.RelativeLayout")),
+                                1)))
+                .atPosition(0);
+        relativeLayout2.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.reminder_title), withText("Test"),
+                        childAtPosition(
+                                allOf(withId(R.id.add_reminder_layout_top),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("Test123"));
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.reminder_title), withText("Test123"),
+                        childAtPosition(
+                                allOf(withId(R.id.add_reminder_layout_top),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        appCompatEditText3.perform(closeSoftKeyboard());
+
+        ViewInteraction actionMenuItemView2 = onView(
+                allOf(withId(R.id.save_reminder), withContentDescription("Save Medicine"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        2),
+                                1),
+                        isDisplayed()));
+        actionMenuItemView2.perform(click());
+
+        DataInteraction relativeLayout3 = onData(anything())
+                .inAdapterView(allOf(withId(R.id.list),
+                        childAtPosition(
+                                withClassName(is("android.widget.RelativeLayout")),
+                                1)))
+                .atPosition(0);
+        relativeLayout3.perform(click());
+
+        ViewInteraction actionMenuItemView3 = onView(
+                allOf(withId(R.id.discard_reminder), withContentDescription("Discard Medicine"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        2),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView3.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(android.R.id.button1), withText("Delete"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        appCompatButton.perform(scrollTo(), click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
